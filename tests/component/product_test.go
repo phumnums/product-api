69fd2de5db6a4b624e8f79d585e96ded5a450c82
Sirintra Phumnum <ph.sirintra@gmail.com>
2026-06-02 13:41:12 +0700

    test: add component tests for patch product

--- tests/component/product_test.go
+++ tests/component/product_test.go
@@ -8,8 +8,10 @@ import (
 	"testing"
 
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/oklog/ulid/v2"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"main.go/internal/domain"
 	"main.go/internal/handler"
 	"main.go/internal/repository"
 	"main.go/internal/router"
@@ -76,6 +78,103 @@ func TestCreateProductComponent_InvalidRequest(t *testing.T) {
 
 	req := httptest.NewRequest(
 		http.MethodPost, "/product",
+		strings.NewReader(body),
+	)
+	req.Header.Set("Content-Type", "application/json")
+
+	res := httptest.NewRecorder()
+
+	r.ServeHTTP(res, req)
+
+	assert.Equal(t, http.StatusBadRequest, res.Code)
+	assert.Contains(t, res.Body.String(), `"successful":false`)
+	assert.Contains(t, res.Body.String(), `"error_code":"invalid request"`)
+}
+
+func TestPatchProductComponent_Success(t *testing.T) {
+	r := setupTestRouter(t)
+	db := setupTestDB(t)
+	repo := repository.NewProductPostgresRepository(db)
+
+	product := &domain.Product{
+		ID:    ulid.Make().String(),
+		Name:  "Johnson's Baby",
+		Price: float64(1680),
+	}
+
+	created, err := repo.Create(context.Background(), product)
+	require.NoError(t, err)
+
+	body := `{
+		"name": "Johnson's Baby",
+		"price": 1700
+	}`
+
+	req := httptest.NewRequest(
+		http.MethodPatch,
+		"/product/"+created.ID,
+		strings.NewReader(body),
+	)
+	req.Header.Set("Content-Type", "application/json")
+
+	res := httptest.NewRecorder()
+
+	r.ServeHTTP(res, req)
+
+	assert.Equal(t, http.StatusOK, res.Code)
+	assert.Contains(t, res.Body.String(), `"successful":true`)
+	assert.Contains(t, res.Body.String(), `"error_code":""`)
+
+	updated, err := repo.GetProductByID(context.Background(), created.ID)
+	assert.NoError(t, err)
+	assert.Equal(t, "Johnson's Baby", updated.Name)
+	assert.Equal(t, float64(1700), updated.Price)
+}
+
+func TestPatchProductComponent_NotFound(t *testing.T) {
+	r := setupTestRouter(t)
+
+	body := `{
+		"name": "Johnson's Baby",
+		"price": 1700
+	}`
+
+	req := httptest.NewRequest(
+		http.MethodPatch,
+		"/product/TESTPRODUCTNOTFOUND",
+		strings.NewReader(body),
+	)
+	req.Header.Set("Content-Type", "application/json")
+
+	res := httptest.NewRecorder()
+
+	r.ServeHTTP(res, req)
+
+	assert.Equal(t, http.StatusNotFound, res.Code)
+	assert.Contains(t, res.Body.String(), `"successful":false`)
+	assert.Contains(t, res.Body.String(), `"error_code":"product not found"`)
+}
+
+func TestPatchProductComponent_EmptyBody(t *testing.T) {
+	r := setupTestRouter(t)
+
+	db := setupTestDB(t)
+	repo := repository.NewProductPostgresRepository(db)
+
+	product := &domain.Product{
+		ID:    ulid.Make().String(),
+		Name:  "Johnson's Baby",
+		Price: float64(1680),
+	}
+
+	created, err := repo.Create(context.Background(), product)
+	require.NoError(t, err)
+
+	body := `{}`
+
+	req := httptest.NewRequest(
+		http.MethodPatch,
+		"/product/"+created.ID,
 		strings.NewReader(body),
 	)
 	req.Header.Set("Content-Type", "application/json")
